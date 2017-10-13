d76add86bcc4eac440f278dc57689e7cb0e50f77
Luiz Carlos Rodrigues Chaves <lucachaves@gmail.com>
2017-10-13 20:49:53 -0300

    Solução do Prime factors

--- codes/ecma/tasks/responses/array/prime-factors.js
+++ codes/ecma/tasks/responses/array/prime-factors.js
@@ -1,6 +1,19 @@
 
-function primeFactors(number) {
-  // TODO
+function isPrime(number) {
+  for(let i = 2, s = Math.sqrt(number); i <= s; i++)
+    if(number % i === 0) return false;
+  return number !== 1;
 }
 
-module.exports primeFactors
+function primeFactors(number) {
+  let factors = []
+  for (let i = 2; i <= number; i++) {
+      while ((number % i) === 0) {
+          factors.push(i)
+          number /= i
+      }
+  }
+  return factors
+}
+
+module.exports = primeFactors
--- codes/ecma/tasks/responses/array/prime-factors.test.js
+++ codes/ecma/tasks/responses/array/prime-factors.test.js
@@ -5,43 +5,43 @@ describe('Prime Factors', function () {
     expect(primeFactors(1)).toEqual([])
   })
 
-  test.skip('factoring 2', function () {
+  test('factoring 2', function () {
     expect(primeFactors(2)).toEqual([2])
   })
 
-  test.skip('factoring 3', function () {
+  test('factoring 3', function () {
     expect(primeFactors(3)).toEqual([3])
   })
 
-  test.skip('factoring 4', function () {
+  test('factoring 4', function () {
     expect(primeFactors(4)).toEqual([2, 2])
   })
 
-  test.skip('factoring 6', function () {
+  test('factoring 6', function () {
     expect(primeFactors(6)).toEqual([2, 3])
   })
 
-  test.skip('factoring 8', function () {
+  test('factoring 8', function () {
     expect(primeFactors(8)).toEqual([2, 2, 2])
   })
 
-  test.skip('factoring 9', function () {
+  test('factoring 9', function () {
     expect(primeFactors(9)).toEqual([3, 3])
   })
 
-  test.skip('factoring 27', function () {
+  test('factoring 27', function () {
     expect(primeFactors(27)).toEqual([3, 3, 3])
   })
 
-  test.skip('factoring 625', function () {
+  test('factoring 625', function () {
     expect(primeFactors(625)).toEqual([5, 5, 5, 5])
   })
 
-  test.skip('factoring 901255', function () {
+  test('factoring 901255', function () {
     expect(primeFactors(901255)).toEqual([5, 17, 23, 461])
   })
 
-  test.skip('factoring 93819012551', function () {
+  test('factoring 93819012551', function () {
     expect(primeFactors(93819012551)).toEqual([11, 9539, 894119])
   })
 })
