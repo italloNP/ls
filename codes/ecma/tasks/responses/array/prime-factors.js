b4a7529a58c3634961da669880afad54de542479
Luiz Carlos Rodrigues Chaves <lucachaves@gmail.com>
2017-10-13 21:19:44 -0300

    Refatorando Prime factors

--- codes/ecma/tasks/responses/array/prime-factors.js
+++ codes/ecma/tasks/responses/array/prime-factors.js
@@ -1,17 +1,11 @@
 
-function isPrime(number) {
-  for(let i = 2, s = Math.sqrt(number); i <= s; i++)
-    if(number % i === 0) return false;
-  return number !== 1;
-}
-
 function primeFactors(number) {
   let factors = []
   for (let i = 2; i <= number; i++) {
-      while ((number % i) === 0) {
-          factors.push(i)
-          number /= i
-      }
+    while ((number % i) === 0) {
+        factors.push(i)
+        number /= i
+    }
   }
   return factors
 }
