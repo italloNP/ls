0742dcb44462d2a03bdb1c0a45981b674d5eb72a
Luiz Carlos Rodrigues Chaves <lucachaves@gmail.com>
2017-10-25 06:18:50 -0300

    task produtos array

--- codes/ecma/tasks/responses/array/products.test.js
+++ codes/ecma/tasks/responses/array/products.test.js
@@ -2,13 +2,13 @@ const total = require('./products')
 
 describe('Cart Tool', () => {
 
-  const produtos = [
+  const products = [
     ['Bicicleta', 1200.0],
     ['Capacete', 450.0]
   ]
 
   test('counting total cart', () => {
-    expect(total(produtos)).toBe(1650.0)
+    expect(total(products)).toBe(1650.0)
   })
 
 })
